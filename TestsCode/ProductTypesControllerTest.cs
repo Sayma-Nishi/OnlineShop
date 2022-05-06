08c8f5f0fc006ad817124f9268dd06875f7635cd
Sayma-Nishi <103818688+Sayma-Nishi@users.noreply.github.com>
2022-05-06 16:37:16 +0600

    Add files via upload

---
+++ TestsCode/ProductTypesControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OnlineShop.Controllers;
+using OnlineShop;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using OnlineShop.Areas.Admin.Controllers;
+
+namespace TestsCode
+{
+    [TestClass]
+    public class ProductTypesControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            //arrange
+            ProductTypesController controller = new ProductTypesController();
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+
+            //Assert
+
+        }
+
+       
+
+
+    }
+
+}
