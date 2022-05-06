f688dfba78118c2b03893fe50de38d84ea19bd3e
Sayma-Nishi <103818688+Sayma-Nishi@users.noreply.github.com>
2022-05-06 16:35:56 +0600

    Add files via upload

---
+++ TestsCode/ControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OnlineShop.Controllers;
+using OnlineShop;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+
+namespace TestsCode
+{
+    [TestClass]
+    public class ControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            //arrange
+            HomeController controller = new HomeController();
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+
+            //Assert
+
+        }
+
+        public void Privacy()
+        {
+            HomeController controller = new HomeController();
+            ViewResult result = controller.Privacy() as ViewResult;
+            Assert.IsNotNull (result);
+        }
+        
+
+        }
+
+    }
